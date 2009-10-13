04c125d75fcedac175f28743fcb55f7307f64bbf
Andrew Bush <andrew.bush@pobox.com>
2009-10-13 15:05:29 -0500

    order_by and then_by

--- prep/exercise_to_complete_for_day_1/product/nothinbutdotnetprep/infrastructure/sorting/SortExtensions.cs
+++ prep/exercise_to_complete_for_day_1/product/nothinbutdotnetprep/infrastructure/sorting/SortExtensions.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using nothinbutdotnetprep.infrastructure.extensions;
 
 namespace nothinbutdotnetprep.infrastructure.sorting
 {
@@ -15,6 +16,16 @@ namespace nothinbutdotnetprep.infrastructure.sorting
         public static IComparer<T> followed_by<T>(this IComparer<T> first_comparer, IComparer<T> second_comparer)
         {
             return new ChainedComparer<T>(first_comparer, second_comparer);
+        }
+
+        public static IEnumerable<ItemToSort> order_by<ItemToSort, ItemProperty>(this IEnumerable<ItemToSort> items, Func<ItemToSort, ItemProperty> property_accessor) where ItemProperty: IComparable<ItemProperty>
+        {
+            return items.sort(new PropertyComparer<ItemToSort, ItemProperty>(property_accessor));
+        }
+
+        public static IEnumerable<ItemToSort> then_by<ItemToSort, ItemProperty>(this IEnumerable<ItemToSort> items, Func<ItemToSort, ItemProperty> property_accessor) where ItemProperty : IComparable<ItemProperty>
+        {
+            return order_by(items, property_accessor);
         }
     }
 }
