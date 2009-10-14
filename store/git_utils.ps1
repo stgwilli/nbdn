6631e43b57d04d2a29070c3cc6917a38b02c7253
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2009-10-14 13:35:45 -0600

    Adding more git utilities

--- store/git_utils.ps1
+++ store/git_utils.ps1
@@ -4,6 +4,21 @@ function merge($working_branch,$branch_to_merge)
   git merge $branch_to_merge
 }
 
+function delete_branch($branch)
+{
+  git branch -D $branch 
+}
+function checkout($branch)
+{
+   git checkout $branch
+
+   if ($LastExitCode -ne 0)
+   {
+     git checkout -b $branch
+   }
+  
+}
+
 function commit($message)
 {
   git add -A
