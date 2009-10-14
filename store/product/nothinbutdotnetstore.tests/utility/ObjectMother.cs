498275d100413621d5bd62244338eca0c7223ec2
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2009-10-13 21:29:01 -0600

    Updated namespaces

--- store/product/nothinbutdotnetstore.tests/utility/ObjectMother.cs
+++ store/product/nothinbutdotnetstore.tests/utility/ObjectMother.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Web;
-using nothinbutdotnetstore.web.core;
 using System.Linq;
 
 namespace nothinbutdotnetstore.tests.utility
