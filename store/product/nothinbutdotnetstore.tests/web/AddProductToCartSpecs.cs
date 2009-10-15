f486321478ccfd414d5428250ecb1ccfa4db5c3b
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2009-10-15 09:17:07 -0600

    Updated the AddProductToCartSpecs

--- store/product/nothinbutdotnetstore.tests/web/AddProductToCartSpecs.cs
+++ store/product/nothinbutdotnetstore.tests/web/AddProductToCartSpecs.cs
@@ -1,51 +1,47 @@
- using developwithpassion.bdd.contexts;
- using developwithpassion.bdd.harnesses.mbunit;
- using developwithpassion.bdd.mocking.rhino;
- using developwithpassion.bdddoc.core;
- using nothinbutdotnetstore.dto;
- using nothinbutdotnetstore.tasks;
- using nothinbutdotnetstore.web.application;
- using nothinbutdotnetstore.web.core;
- using Rhino.Mocks;
+using developwithpassion.bdd.contexts;
+using developwithpassion.bdd.harnesses.mbunit;
+using developwithpassion.bdd.mocking.rhino;
+using developwithpassion.bdddoc.core;
+using nothinbutdotnetstore.dto;
+using nothinbutdotnetstore.tasks;
+using nothinbutdotnetstore.web.application;
+using nothinbutdotnetstore.web.core;
+using Rhino.Mocks;
 
 namespace nothinbutdotnetstore.tests.web
- {   
-     public class AddProductToCartSpecs
-     {
-         public abstract class concern : observations_for_a_sut_with_a_contract<ApplicationWebCommand,
-                                             AddProductToCart>
-         {
-        
-         }
+{
+    public class AddProductToCartSpecs
+    {
+        public abstract class concern : observations_for_a_sut_with_a_contract<ApplicationWebCommand,
+                                            AddProductToCart> {}
 
-         [Concern(typeof(AddProductToCart))]
-         public class when_adding_a_product_to_cart : concern
-         {
-             context c = () =>
-                             {
-                                 request = an<Request>();
-                                 catalog_tasks = the_dependency<CatalogTasks>();
-                                 product = new Product();
+        [Concern(typeof (AddProductToCart))]
+        public class when_adding_a_product_to_cart : concern
+        {
+            context c = () =>
+            {
+                request = an<Request>();
+                catalog_tasks = the_dependency<CatalogTasks>();
+                product = new Product();
 
 
-                                 request.Stub(x => x.map<Product>()).Return(product);
-                             };
+                request.Stub(x => x.map<Product>()).Return(product);
+            };
 
-             because b = () =>
-                             {
-                                sut.process(request);
-                             };
+            because b = () =>
+            {
+                sut.process(request);
+            };
 
-        
-             it should_be_told_to_add_a_product_to_the_cart = () =>
-                        {
-                            catalog_tasks.received(x => x.add_to_cart(product));
-                        };
 
-             private static Request request;
-             private static CatalogTasks catalog_tasks;
-             private static Product product;
-         }
+            it should_be_told_to_add_a_product_to_the_cart = () =>
+            {
+                catalog_tasks.received(x => x.add_to_cart(product));
+            };
 
-     }
- }
+            static Request request;
+            static CatalogTasks catalog_tasks;
+            static Product product;
+        }
+    }
+}
